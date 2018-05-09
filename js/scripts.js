ac8069934187a7d457cdd1c3c1c0fa639e411200
moskowitza <moskowitzaaron@mac.com>
2018-05-09 12:38:50 -0400

    on to video 3

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var cast = {
+    "characters":[
+        {
+            "name":"Dark Phoenix",
+            "shortCode":"dark-phoenix",
+            "realName":"Jean Grey",
+            "team":"X-men"
+        },
+        {
+            "name":"Spider-Man",
+            "shortCode":"spider-man",
+            "realName":"Peter Parker",
+            "team":"none"
+        },
+        {
+            "name":"Iron Man",
+            "shortCode":"iron-man",
+            "realName":"Tony Stark",
+            "team":"Avengers"
+        },
+        {
+            "name":"Cyclops",
+            "shortCode":"cyclops",
+            "realName":"Scott Summers",
+            "team":"X-men"
+        }
+    ]
+}
